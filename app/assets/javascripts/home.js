c0c36567890d3580b84b234651bb090a912b7b8b
calderonroberto <roberto@robertocalderon.ca>
2013-03-10 16:13:56 -0700

    using api instead of cookies

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -12,10 +12,11 @@ function initState () {
 function stateHandler (json) {
   var apps = json.apps;
   var staged_app = json.staged_app;
+  var display_id = json.display.id;
   var thingbroker_url = encodeURIComponent(json.setup.thingbroker_url)
   var messages = json.messages;
   if (typeof apps != 'undefined' ){ updateAppMenu(apps) };
-  if (typeof staged_app != 'undefined' && staged_app != null){ updateAppContainer(staged_app, thingbroker_url) };
+  if (typeof staged_app != 'undefined' && staged_app != null){ updateAppContainer(staged_app, display_id, thingbroker_url) };
   if (typeof messages != 'undefined' ){ updateMessages(messages) };
 };
 
@@ -33,11 +34,11 @@ $(function() {
 */
 });
 
-function updateAppContainer (staged_app, thingbroker_url) {
+function updateAppContainer (staged_app, display_id, thingbroker_url) {
   if ($('.appcontainer').attr('id') != staged_app.id) {
     $(".appcontainer").attr('id', staged_app.id);
-    var display=getCookie("display_id");
-    $(".appcontainer").attr('src', staged_app.url+"?display_id="+display+"&thingbroker_url="+thingbroker_url);
+    //var display=getCookie("display_id");    
+    $(".appcontainer").attr('src', staged_app.url+"?display_id="+display_id+"&thingbroker_url="+thingbroker_url);
   }
 }
 
