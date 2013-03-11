05af7b95976dbf4832dcd19eaa98f9d90f1599d1
calderonroberto <roberto@robertocalderon.ca>
2013-03-11 02:07:19 -0700

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
  
-#put all our needed gems in vendors :) 
+#put all our needed gems in vendors 
+echo building gems
 bundle install --path vendor/bundle
 
 #migrate anything new
+echo running migrations
 bundle exec rake db:migrate
 bundle exec rake db:migrate RAILS_ENV="production" 
 
-
 #remove previous assets
+echo removing previous assets
 rm -R /var/www/container/public/assets/*
  
 # precompile assets (if you need to debug delete all in /public/assets)
+echo precompiling assets
 bundle exec rake assets:precompile --trace RAILS_ENV=production
 
 #change permissions
+echo ensuring permissions
 chown -R www-data:www-data /var/www/container/
 
 #reset server
+echo killing nginx
 killall nginx
+echo restarting nginx
 /etc/init.d/nginx restart
 
