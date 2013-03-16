bb0adbe917d36dd699ca381e7da7970402e90596
calderonroberto <roberto@robertocalderon.ca>
2013-03-16 11:46:35 -0700

    solved error on messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,12 +2,12 @@ class MessagesController < ApplicationController
 
 def new
  @display_id = params[:id]
- @message = Display.find(@display_id).messages.build
+ @message = Display.find_by_unique_id(@display_id).messages.build
  render :layout => 'mobile'
 end
 
 def create
-  display = Display.find(params[:message][:display_id])
+  display = Display.find_by_unique_id(params[:message][:display_id])
   @display_id = display.id  
   @message = display.messages.build(from: params[:message][:from], message: params[:message][:message])
   if @message.save
