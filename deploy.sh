fff3bbcb413e27dcecd0e9fb13799fb2636daa4a
calderonroberto <roberto@robertocalderon.ca>
2014-04-10 15:44:35 -0700

    new deploy

--- deploy.sh
+++ deploy.sh
@@ -21,11 +21,19 @@ bundle exec rake assets:precompile --trace RAILS_ENV=production
 echo ensuring permissions
 chown -R www-data:www-data ../container/
 
+#killprevious instances
+echo killing all previous ruby
+killall ruby
+
 #reset server
 echo killing nginx
 killall nginx
 echo restarting nginx
 /etc/init.d/nginx restart
+
+#restart workers
+echo starting workers with foreman -f Procfile
+foreman start -f Procfile &
 
 #echo restarting workers
 #cd /var/www/icd
