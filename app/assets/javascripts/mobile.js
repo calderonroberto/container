c7fb6cfc6a1d7af28df902a1b46ed12f9d0290c4
roberto <roberto.entrepreneur@gmail.com>
2014-05-03 22:02:58 -0700

    fixed navbar on mobile

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,6 +1,8 @@
 //= require jquery
 //A handy screen resizing function
+/*
 $(document).ready(function() { 
   $(".mobileappcontainer").width(screen.width);
   $(".mobileappcontainer").height(screen.height-80);
 });
+*/
