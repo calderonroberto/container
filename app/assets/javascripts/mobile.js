88b125411f19cffe15ebb7780c9f62940fc340b0
roberto <roberto.entrepreneur@gmail.com>
2014-05-03 22:20:57 -0700

    fixed navbar on mobile

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -1,8 +1,21 @@
 //= require jquery
-//A handy screen resizing function
-/*
-$(document).ready(function() { 
-  $(".mobileappcontainer").width(screen.width);
-  $(".mobileappcontainer").height(screen.height-80);
+//A handy screen resizing function. This will resize the iframe for the mobile
+// app to the screen size. Bootstrap will resize this, so in show.html.erb
+// we will make sure it's corrected if changed.
+// Bootstrap overrides our attempts to properly resize the iframe with css.
+// On some devices bootstrap utilizes the device max-screen capabilities (e.g. 700+)
+// but zooms at 350px, leaving some of the iframe out. I had to use some jquery
+// to solve this.
+
+$('document').ready(function() {
+   window.updateIframe = function() {
+     var h = $(window).height();
+     var w = $(window).width();
+     $(".mobileappcontainer").height(h - 80);
+     $(".mobileappcontainer").width(w);
+   }
+setTimeout(function(){
+ window.updateIframe();
+}, 1000);
 });
-*/
+
