2416dc0a492b36c1575d898dd9500f8f13a9ee42
calderonroberto <roberto@robertocalderon.ca>
2015-02-18 20:54:10 -0800

    initial upgrade

--- Gemfile
+++ Gemfile
@@ -1,24 +1,27 @@
 # You will need ruby 1.9.3p0 and rails 3.2.8
 source 'https://rubygems.org'
 
-ruby "1.9.3"
-gem 'rails', '3.2.6'
+# Upgradding from:
+#ruby "1.9.3"
+#gem 'rails', '3.2.6'
+
+ruby "2.2.0"
+gem 'rails', '4.2.0'
 
 #jquery
-gem 'jquery-rails', '2.1.1'
+gem 'jquery-rails'#, '2.1.1'
 
 # bootstrap gem lacks mobile support.
 # but complementing for will_paginate:
-gem 'bootstrap-will_paginate', '0.0.7'
+gem 'bootstrap-will_paginate'#, '0.0.7'
 
-gem 'will_paginate', '3.0.3'
-gem 'bcrypt-ruby', '3.0.1'
+gem 'will_paginate'#, '3.0.3'
+gem 'bcrypt-ruby'#, '3.0.1'
 
-gem 'faker', '1.2.0'
+gem 'faker'#, '1.2.0'
 
-#testing faye
-gem 'faye', '0.8.3'
-gem 'thin', '1.4.1'
+gem 'faye'#, '0.8.3'
+gem 'thin'#, '1.4.1'
 
 #resque workers
 gem 'resque','1.22.0', :require => 'resque/server'
@@ -26,52 +29,53 @@ gem 'resque','1.22.0', :require => 'resque/server'
 #foreman export, use with: "sudo bundle exec foreman export initscript /etc/init.d -u root -a icd"
 # then, to add: http://www.debian-administration.org/article/28/Making_scripts_run_at_boot_time_with_Debian
 gem 'foreman-export-initscript' #', :git => 'git://github.com/lzgo/foreman-export-initscript.git'
-gem 'foreman', '0.62.0'
+gem 'foreman'#, '0.62.0'
 
 #to deploy in heroku
-gem 'pg', '0.14.1'
+gem 'pg'#, '0.14.1'
 
-#testing fileuploads with s3w
-gem 'paperclip', '3.4.1'
-gem 'aws-sdk', '1.8.5'
-gem 'fog', '1.10.1'
+#fileuploads with s3w
+gem 'paperclip'#, '3.4.1'
+gem 'aws-sdk'#, '1.8.5'
+gem 'fog'#, '1.10.1'
 
 #For Facebook signin
-gem 'devise', '3.2.1'
-gem 'omniauth', '1.1.4'
-gem 'omniauth-facebook', '1.5.0'
-gem 'oauth2', '0.8.1'
+gem 'devise'#, '3.2.1'
+gem 'omniauth'#, '1.1.4'
+gem 'omniauth-facebook'#, '1.5.0'
+gem 'oauth2'#, '0.8.1'
 
 #Facebook data (friends
-gem 'koala', '1.8.0'
+gem 'koala'#, '1.8.0'
 
 #To make POST requests
-gem 'faraday', '0.9.0'
+gem 'faraday'#, '0.9.0'
 
 #To allow profanity filter
-gem 'obscenity', '1.0.2'
+gem 'obscenity'#, '1.0.2'
 
 # Gems used only for assets and not required
 # in production environments by default.
+#RVCA: rails4 does not use group assets
 group :assets do
-  gem 'sass-rails',   '~> 3.2.3'
-  gem 'coffee-rails', '~> 3.2.1'
-  gem 'uglifier', '>= 1.0.3'
+  gem 'sass-rails'#,   '~> 3.2.3'
+  gem 'coffee-rails'#, '~> 3.2.1'
+  gem 'uglifier'#, '>= 1.0.3'
 end
 
 group :test do
-  gem 'capybara', '1.1.2' 
-  gem 'factory_girl_rails', '1.4.0' 
+  gem 'capybara'#, '1.1.2' 
+  gem 'factory_girl_rails'#, '1.4.0' 
 #  gem 'database_cleaner', '0.8.0' #works if transactional_fixtures=false
 end
 
 group :production do
   #gem 'mysql', '2.8.1'
-  gem 'mysql2', '0.3.11'
+  gem 'mysql2'#, '0.3.11'
 end
 
 group :development, :test do
-  gem 'sqlite3', '1.3.5'
-  gem 'rspec-rails','2.10.0'
+  gem 'sqlite3'#, '1.3.5'
+  gem 'rspec-rails'#,'2.10.0'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,89 +1,20 @@
-Container::Application.routes.draw do
+Rails.application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
 
-  resources :apps, only: [:new, :create, :destroy, :update, :edit]
-  resources :sessions, only: [:new, :create]
-  resources :displays, only: [:new, :create]
-  resources :subscriptions, only: [:create, :destroy]
-  resources :notes, only: [:create]
-  resources :messages, only: [:create]
-  resources :stagings, only: [:create]
-  resources :checkins, only: [:create, :show] #TODO: REMOVE SHOW
-  resources :gifts, only: [:create]
-  resources :favours, only: [:create]
-   match '/favours/reciprocate', to: 'favours#reciprocate' #added new method for reciprocate form
-  resources :setups, only: [:update, :edit]
-  resources :users, only:[:create]
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
 
-  root :to => 'static_pages#home'  
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
 
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  match '/admin',   to: 'admin#home'
-  match '/signup',  to: 'displays#new'
-  match '/signin',  to: 'sessions#new'
-  match '/signout', to: 'sessions#destroy'
-
-  #large display
-  match '/welcome', to: 'static_pages#welcome'
-  match '/home', to: 'static_pages#home'
-  match '/content/:id', to: 'static_pages#content' #large display 
-
-  #mobile display
-  match 'm/:display_name', to: 'mobile#index', :as => :m #To render apps on mobile login
-  match '/mobile/:id', to: 'mobile#index', :as => :mobile 
-  
-  match '/mobile/app/:app_id', to: 'mobile#show', :as => :mobile_app
-  match '/notes/new/:id', to: 'notes#new', :as => :notes_new #for particular display_id
-  match '/messages/new/:id', to: 'messages#new', :as => :messages_new #for particular display_id
-  match '/people/:id', to: 'users#index', :as => :people
-  #match '/people/user/:id', to: 'users#show', :as => :people_user #TODO: Delete
-  match '/checkins/:id', to: 'checkins#show', :as => :checkins
-  match '/profile', to: 'users#profile', :as => :profile
-  match '/help', to: 'help#show', :as => :help
-
-  match '/anonymouscheckin', to: 'checkins#anonymous', :as => :checkin_anonymous #TODO: change this to something with better meaning
-
-
-
-  #api
-  match '/api/state', to: 'states#state', :as => :api_state
-  match '/api/:display_id/state', to: 'states#statedisplay', :as => :api_display_state
-  match '/api/apps', to: 'apps#apps', :as => :api_apps
-  match '/api/:display_id/analytics', to: 'analytics#show', :as => :api_display_analytics
-  match '/api/:display_id/scores', to: 'scores#show', :as => :api_display_scores
-  match '/api/:display_id/checkins', to: 'checkins#displaycheckins', :as => :api_display_checkins
-  match '/api/currentuser', to: 'users#currentuser', :as => :api_currentuser
-
-
-  #resque web server
-  mount Resque::Server, :at => "/resque"
-
-  #setting routes for devise. Creates the methods:
-  # user_omniauth_authorize_path(provider)
-  # user_omniauth_callback_path(provider)
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  
-  match '/signinuser',  to: 'sessions#newuser'
-  match '/signoutuser', to: 'sessions#destroyuser', :as => :signoutuser
-
-  # THIS MUST BE THE LAST ROUTE!
-  #404 errors
-  match '*a', :to => 'errors#routing'
-  
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  # Sample resource route with options:
+  # Example resource route with options:
   #   resources :products do
   #     member do
   #       get 'short'
@@ -95,34 +26,31 @@ Container::Application.routes.draw do
   #     end
   #   end
 
-  # Sample resource route with sub-resources:
+  # Example resource route with sub-resources:
   #   resources :products do
   #     resources :comments, :sales
   #     resource :seller
   #   end
 
-  # Sample resource route with more complex sub-resources
+  # Example resource route with more complex sub-resources:
   #   resources :products do
   #     resources :comments
   #     resources :sales do
-  #       get 'recent', :on => :collection
+  #       get 'recent', on: :collection
   #     end
   #   end
 
-  # Sample resource route within a namespace:
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
